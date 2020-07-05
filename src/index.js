72abf4e5f77e3305ac0d7e54834fa3024f4fa934
Renan Albuquerque <62337593+ren-prog@users.noreply.github.com>
2020-07-05 11:58:16 -0300

    sequelize config

--- src/index.js
+++ src/index.js
@@ -1,11 +1,22 @@
 const express = require('express');
+const db = require('./models')
 
 const app = express();
+
+const authController = require('./controllers/auth');
+
+// /auth/sing-in
+// /auth/sing-up
+
+app.use('/auth', authController );
+
 
 app.get('/', (req, res)=> {
     return res.json('Api runing');
 })
 
-app.listen(3333, () => {
-    console.log('Listening on port 3333')
+db.sequelize.sync().then(()=> {
+    app.listen(3333, () => {
+        console.log('Listening on port 3333')
+    });
 });
