16b8adfc5bd4b7ad93f029aa38a6b8ee5e47458f
Renan Albuquerque <62337593+ren-prog@users.noreply.github.com>
2020-07-11 23:10:55 -0300

    Excluindo auth dos JWT

--- src/middlewares/jwt.js
+++ src/middlewares/jwt.js
@@ -1,11 +1,19 @@
 const { verifyJwt } = require('../helpers/jwt');
 
 const checkJwt = (req, res, next ) => {
+    //auth/sing-in
+    //auth/sing-up
+    const { url: path } = req;
+
+    const excludePaths = ['auth/sing-in', 'auth/sing-up'];
+    const isExcluded = !!excludePaths.find((p) => p.startsWith(path));
+
+    console.log(path, isExcluded);
 
     let token = req.headers['authorization'];
     token = token ? token.slice(7, token.lenght) : null;
 
-    if(!token) res.jsonUnauthorize(null, 'Invalid Token');
+    if(!token) res.jsonUnauthorized(null, 'Invalid Token');
 
     try {
         
@@ -17,12 +25,11 @@ const checkJwt = (req, res, next ) => {
 
         req.accountId = decoded.id;
         next();
-        
+
     } catch (error) {
         res.jsonUnauthorize(null, 'Invalid Token');
     }
     
-
 };
 
 module.exports = checkJwt;
